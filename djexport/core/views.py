b1d0ab297610636db699f8cfa3191cea9edbf6de
Marcos Cardoso <vrcmarcos@gmail.com>
2016-09-06 22:34:28 -0300

    Ajuste de campos para export via view person/export

--- djexport/core/views.py
+++ djexport/core/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy as r
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView
 from django.views.generic import UpdateView, DeleteView
 from import_export.admin import ExportMixin
 from import_export.formats.base_formats import XLSX
+
+from djexport.core.admin import PersonResource
+from .forms import PersonForm
 from .mixins import NameSearchMixin
 from .models import Person
-from .forms import PersonForm
-from django.http import HttpResponse
 
 
 def home(request):
@@ -30,24 +32,20 @@ person_delete = DeleteView.as_view(
 
 
 def export_data_person(request):
-    e = ExportMixin()
-    e.model = Person
-    file_format = XLSX()
     queryset = Person.objects.all()
-    data = e.get_export_data(file_format, queryset)
-    response = HttpResponse(
-        data, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    response['Content-Disposition'] = 'attachment; filename="person.xlsx"'
-    return response
+    return _export_data(queryset, 'person')
 
 
 def export_data_person_blocked(request):
+    queryset = Person.objects.filter(blocked=True)
+    return _export_data(queryset, 'person_blocked')
+
+
+def _export_data(queryset, filename_prefix):
     e = ExportMixin()
+    e.resource_class = PersonResource
     e.model = Person
-    file_format = XLSX()
-    queryset = Person.objects.all().filter(blocked=True)
-    data = e.get_export_data(file_format, queryset)
-    response = HttpResponse(
-        data, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    response['Content-Disposition'] = 'attachment; filename="person_blocked.xlsx"'
+    data = e.get_export_data(XLSX(), queryset)
+    response = HttpResponse(data, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="{0}.xlsx"'.format(filename_prefix)
     return response
